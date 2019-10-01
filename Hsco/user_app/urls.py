8c991cb750b14c8adde212192ddbf45a4346fe8f
VIKAS <vikas.pandey9323@gmail.com>
2019-10-01 16:44:11 +0530

    Second Commit all set

--- Hsco/user_app/urls.py
+++ Hsco/user_app/urls.py
@@ -17,13 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 
-from . import settings
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('user_app.urls')),
+path('', views.home, name='home')
 
 ]
-if settings.DEBUG:
-	urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
