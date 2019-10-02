8c66a598b135bfd219ddb7d9db67ca406ee9886c
theambrosial <47419028+theambrosial@users.noreply.github.com>
2019-10-02 14:11:47 +0530

    harsh ke changes....

--- Hsco/user_app/views.py
+++ Hsco/user_app/views.py
@@ -2,3 +2,32 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"dashboardnew/dash.html",)
+
+
+def cust_mod_form(request):
+    return render(request,"forms/cust_mod_form.html",)
+
+def amc_form(request):
+    return render(request,"forms/amc_form.html",)
+
+def dis_mod_form(request):
+    return render(request,"forms/dis_mod_form.html",)
+
+def ess_form(request):
+    return render(request,"forms/ess_form.html",)
+
+def onsite_rep_form(request):
+    return render(request,"forms/onsite_rep_form.html",)
+
+def rep_mod_form(request):
+    return render(request,"forms/rep_mod_form.html",)
+
+def restamping_form(request):
+    return render(request,"forms/restamping_form.html",)
+
+def sidebar(request):
+    return render(request,"base_templates/sidebar.html",)
+
+def navbar(request):
+    return render(request,"base_templates/navbar_for_dashboard.html",)
+
