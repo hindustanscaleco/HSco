a1e828027a4e4ffa0d70d7e2eee125732404afa9
sagar <sagarsingh27998@gmail.com>
2019-10-09 05:43:10 +0530

    customer details form

--- Hsco/customer_app/admin.py
+++ Hsco/customer_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer_Details, Product_Details
+
+admin.site.register(Customer_Details)
+admin.site.register(Product_Details)
