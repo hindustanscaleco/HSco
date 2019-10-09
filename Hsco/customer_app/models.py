8f6f53c976fcd2703a05a986a3e739eea14f7374
sagar <sagarsingh27998@gmail.com>
2019-10-09 07:24:09 +0530

    customer model table corrected

--- Hsco/customer_app/models.py
+++ Hsco/customer_app/models.py
@@ -9,24 +9,17 @@ choices = (('YES', 'YES'),
 
 
 class Customer_Details(models.Model):
-    crn_number = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    crn_number = models.UUIDField()
-    company_name = models.CharField(max_length=80)
-    address = models.CharField(max_length=250)
-    contact_no = models.CharField(max_length=30)
-    customer_email_id = models.EmailField('Email-id', max_length=255, unique=True )
-    date_of_purchase = models.DateField()
-    product_purchase = models.DateTimeField(default=timezone.now)
-    quantity = models.CharField(max_length=30)
-    customer_name = models.CharField(max_length=80,null=True,blank=True)
+    crn_number = models.UUIDField(unique=True, default=uuid.uuid4)
+    customer_name = models.CharField(max_length=80, null=True, blank=True)
     company_name = models.CharField(max_length=80,null=True,blank=True)
     address = models.CharField(max_length=250,null=True,blank=True)
     contact_no = models.CharField(max_length=30,null=True,blank=True)
     customer_email_id = models.CharField(max_length=30,null=True,blank=True)
-    date_of_purchase = models.DateField(null=True,blank=True, default=datetime.date.today)
-    product_purchase_date = models.DateField(null=True,blank=True, default=datetime.date.today)
+    date_of_purchase = models.DateField(null=True,blank=True)
+    product_purchase_date = models.DateTimeField(default=timezone.now,null=True,blank=True)
     #sales_person = models.CharField(max_length=30)
     #new_repeat_purchase = models.CharField(max_length=30)
+
     bill_no = models.CharField(max_length=30,null=True,blank=True)
     upload_op_file = models.FileField(upload_to='',null=True,blank=True)
     po_number = models.CharField(max_length=30,null=True,blank=True)
@@ -36,27 +29,13 @@ class Customer_Details(models.Model):
     value_of_goods = models.CharField(max_length=30,null=True,blank=True)
     channel_of_dispatch = models.CharField(max_length=30,null=True,blank=True)
     notes = models.CharField(max_length=30,null=True,blank=True)
-    feedback_form_filled = models.CharField(max_length=30,null=True,blank=True,choices=choices)
+    feedback_form_filled = models.CharField(max_length=30,null=True,blank=True, choices=choices)
     #dispatch_id_assigned = models.CharField(max_length=30)
-
-
-    def __str__(self):
-        return self.customer_name
-
 
 
 
 class Product_Details(models.Model):
-    product_name = models.CharField(max_length=30)
-    quantity = models.CharField(max_length=30)
-    type_of_scale = models.CharField(max_length=30)
-    model_of_purchase = models.CharField(max_length=30)
-    sub_model = models.CharField(max_length=30)
-    sub_sub_model = models.CharField(max_length=30)
-    serial_no_scale = models.CharField(max_length=80)
-    brand = models.CharField(max_length=30)
-    capacity = models.CharField(max_length=30)
-    unit = models.CharField(max_length=30)
+
     product_name = models.CharField(max_length=30,null=True,blank=True)
     quantity = models.CharField(max_length=30,null=True,blank=True)
     type_of_scale = models.CharField(max_length=30,null=True,blank=True)
