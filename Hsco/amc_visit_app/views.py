7bd99ba35ef13ffebab27869ccfb97208114b18d
theambrosial <47419028+theambrosial@users.noreply.github.com>
2019-10-10 18:04:51 +0530

    made changes in views.py in apps

--- Hsco/amc_visit_app/views.py
+++ Hsco/amc_visit_app/views.py
@@ -1,20 +1,76 @@
 from django.shortcuts import render, redirect
+from .models import Amc_After_Sales
+from .models import Amcvisit
 
 
-def add_customer_details(request):
+def add_Amcvisit(request):
     form = (request.POST or None)
     if request.method == 'POST':
         amcno = request.POST.get('amcno')
+        customer_name = request.POST.get('customer_name')
         company_name = request.POST.get('company_name')
-        address = request.POST.get('address')
 
 
 
-        item = Customer_Details()
+        item = Amcvisit()
 
         item.amcno = amcno
+        item.customer_name = customer_name
         item.company_name = company_name
-        item.company_name = company_name
+
+
+
+        item.save()
+
+        return redirect('/')
+
+
+    context = {
+        'form': form,
+    }
+    return render(request,'forms/amc_form.html',context)
+
+def add_Amc_After_Sales(request):
+    form = (request.POST or None, request.FILES or None)
+    if request.method == 'POST':
+        customer_no = request.POST.get('customer_no')
+        customer_email_id = request.POST.get('customer_email_id')
+        type_of_scale = request.POST.get('type_of_scale')
+        serial_no_scale = request.POST.get('serial_no_scale')
+        contract_valid_in_years = request.POST.get('contract_valid_in_years')
+        contract_amount = request.POST.get('contract_amount')
+        contract_no_reporting_breakdown = request.POST.get('contract_no_reporting_breakdown')
+        contract_start_date = request.POST.get('contract_start_date')
+        contract_end_date = request.POST.get('contract_end_date')
+        visit_1 = request.POST.get('visit_1')
+        repot_1 = request.POST.get('repot_1')
+        visit_2 = request.POST.get('visit_2')
+        repot_2 = request.POST.get('repot_2')
+        visit_3 = request.POST.get('visit_3')
+        repot_3 = request.POST.get('repot_3')
+        visit_4 = request.POST.get('visit_4')
+        repot_4 = request.POST.get('repot_4')
+
+
+        item = Amc_After_Sales()
+
+        item.customer_no = customer_no
+        item.customer_email_id = customer_email_id
+        item.type_of_scale = type_of_scale
+        item.serial_no_scale = serial_no_scale
+        item.contract_valid_in_years = contract_valid_in_years
+        item.contract_amount = contract_amount
+        item.contract_no_reporting_breakdown = contract_no_reporting_breakdown
+        item.contract_start_date = contract_start_date
+        item.contract_end_date = contract_end_date
+        item.visit_1 = visit_1
+        item.repot_1 = repot_1
+        item.visit_2 = visit_2
+        item.repot_2 = repot_2
+        item.visit_3 = visit_3
+        item.repot_3 = repot_3
+        item.visit_4 = visit_4
+        item.repot_4 = repot_4
 
 
 
--- Hsco/ess_app/views.py
+++ Hsco/ess_app/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Ess
 
-# Create your views here.
+
+
+def add_ess(request):
+    form=(request.POST or None)
+    if request.method =='POST':
+        employee_name = request.POST.get('employee_name')
+        details = request.POST.get('details')
+        contact_no = request.POST.get('contact_no')
+        email_id = request.POST.get('email_id')
+        photo = request.POST.get('photo')
+        pancard = request.POST.get('pancard')
+        aadhar_card = request.POST.get('aadhar_card')
+        bank_details = request.POST.get('bank_details')
+        photo_of_cancelled_cheque = request.POST.get('photo_of_cancelled_cheque')
+        calander = request.POST.get('calander')
+        target_of_month = request.POST.get('target_of_month')
+        target_achived_till_now = request.POST.get('target_achived_till_now')
+        month_on_month_sale_achived = request.POST.get('month_on_month_sale_achived')
+        defect_given = request.POST.get('defect_given')
+        warnings_given = request.POST.get('warnings_given')
+
+
+
+        item = Ess()
+
+        item.employee_name = employee_name
+        item.details = details
+        item.contact_no = contact_no
+        item.email_id = email_id
+        item.photo = photo
+        item.pancard = pancard
+        item.aadhar_card = aadhar_card
+        item.photo_of_cancelled_cheque = photo_of_cancelled_cheque
+        item.calander = calander
+        item.target_of_month = target_of_month
+        item.target_achived_till_now = target_achived_till_now
+        item.bank_details = bank_details
+        item.month_on_month_sale_achived = month_on_month_sale_achived
+        item.defect_given = defect_given
+        item.warnings_given = warnings_given
+
+        item.save()
+        return redirect('/')
+
+
+
+    context = {
+        'form': form,
+    }
+    return render(request,'forms/ess_form.html',context)
