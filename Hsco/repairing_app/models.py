7b2f4b6ccb031d8b1b09234f8c170727d1f80fea
theambrosial <47419028+theambrosial@users.noreply.github.com>
2019-10-13 04:11:06 +0530

    added choices in models of repairing app

--- Hsco/repairing_app/models.py
+++ Hsco/repairing_app/models.py
@@ -5,6 +5,52 @@ from django.utils import timezone
 choices = (('YES', 'YES'),
     ('NO', 'NO'),)
 
+choices2 = (('Mechanical', 'Mechanical'),
+            ('Digital', 'Digital'))
+
+choices3 = (('Table Top', 'Table Top'),
+            ('Platform Scale', 'Platform Scale'),
+            ('POS Scales', 'POS Scales'),
+            ('Water Proof Scales', 'Water Proof Scales'),
+            ('Jewellery Scales', 'Jewellery Scales'),
+            ('Lab Scales', 'Lab Scales'),
+            ('Crane Scales', 'Crane Scales'),
+            ('Platform Scales', 'Platform Scales'),
+            ('Mobile Platforms', 'Mobile Platforms'),
+            ('Infant Scales', 'Infant Scales'),
+            ('Baby Cum Adult Scales', 'Baby Cum Adult Scales'),
+            ('Animal Scales', 'Animal Scales'),
+            ('Cattle Scales', 'Cattle Scales'),
+            ('Supra Pall Scales', 'Supra Pall Scales'),
+            )
+
+
+choices4 = (('Scale not turning On', 'Scale not turning On'),
+            ('Scale is showing incorrect weight', 'Scale is showing incorrect weight'),
+            ('Scale only shows 888888','Scale only shows 888888'),
+            ('Scale only shows zero', 'Scale only shows zero'),
+            ('Weight is slowly increasing or','Weight is slowly increasing or'),
+            ('decreasing', 'decreasing'),
+            ('buttons are not working','buttons are not working'),
+            ('Scale doesnt show stable weight','Scale doesnt show stable weight'),
+            ('Display problems (Display gets distorted while showing weight)', 'Display problems (Display gets distorted while showing weight)'),
+            ('Scale is not working on battery or scale is not charging', 'Scale is not working on battery or scale is not charging'),
+            ('Scale turns OFF automatically','Scale turns OFF automatically'),
+            ('Others','Others'),
+            )
+choices5 = (('Display', 'Display'),
+           ('Touch Pad', 'Touch Pad'),
+           ('LED', 'LED'),
+           ('Transformer', 'Transformer'),
+           ('Battery', 'Battery'),
+           ('Switch', 'Switch'),
+           ('Main Cord wire', 'Main Cord wire'),
+           ('Main Circuit Board', 'Main Circuit Board'),
+           ('SMPS', 'SMPS'),
+           ('Load Cell', 'Load Cell'),
+           ('Misc', 'Misc'),
+           )
+
 class Repairing_after_sales_service(models.Model):
     repairingnumber = models.CharField(max_length=40,null=True,blank=True) #combination of pk and 'rep'
     customer_no = models.CharField(max_length=13,null=True,blank=True)
@@ -18,12 +64,12 @@ class Repairing_after_sales_service(models.Model):
     customer_email_id = models.EmailField(max_length=255, unique=True,null=True,blank=True)
     location = models.CharField(max_length=255,null=True,blank=True)
     products_to_be_repaired = models.CharField(max_length=30,null=True,blank=True)
-    type_of_machine = models.CharField(max_length=30,null=True,blank=True)
-    model = models.CharField(max_length=30,null=True,blank=True)
+    type_of_machine = models.CharField(max_length=30,null=True,blank=True, choices=choices2)
+    model = models.CharField(max_length=30,null=True,blank=True, choices=choices3)
     sub_model = models.CharField(max_length=30,null=True,blank=True)
-    problem_in_scale = models.CharField(max_length=255,null=True,blank=True)
-    components_replaced_in_warranty = models.CharField(max_length=255,null=True,blank=True)
-    components_replaced = models.CharField(max_length=255,null=True,blank=True)
+    problem_in_scale = models.CharField(max_length=255,null=True,blank=True,choices=choices4)
+    components_replaced_in_warranty = models.CharField(max_length=255,null=True,blank=True,choices=choices5)
+    components_replaced = models.CharField(max_length=255,null=True,blank=True,choices=choices5)
     replaced_scale_given = models.CharField(choices=choices,default='NO',max_length=30,null=True,blank=True)
     Replaced_scale_serial_no = models.CharField(max_length=60,null=True,blank=True)
     deposite_taken_for_replaced_scale = models.CharField(max_length=60,null=True,blank=True)
