ce03f9dbc2a7084ac2eb104ef7a381398ec3b462
theambrosial <47419028+theambrosial@users.noreply.github.com>
2019-10-15 12:49:24 +0530

    restamping module

--- Hsco/dispatch_app/urls.py
+++ Hsco/dispatch_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 from .views import add_dispatch_details,report_dis_mod, manager_view
 
 urlpatterns = [
-    path('add_dispatch_details/',add_dispatch_details, name ='add_dispatch_details'),
-    path('report_dis_mod/',report_dis_mod, name ='report_dis_mod'),
-    path('manager_view/',manager_view, name ='manager_view'),
+    path('add_dispatch_details/', add_dispatch_details, name ='add_dispatch_details'),
+    path('report_dis_mod/', report_dis_mod, name ='report_dis_mod'),
+    path('manager_view/', manager_view, name ='manager_view'),
 ]
