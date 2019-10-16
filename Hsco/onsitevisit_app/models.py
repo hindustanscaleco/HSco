a0bc3ae2e3dd2680dec886d1ff33967ec3c35fda
theambrosial <47419028+theambrosial@users.noreply.github.com>
2019-10-16 13:55:21 +0530

    onsite completed

--- Hsco/onsitevisit_app/models.py
+++ Hsco/onsitevisit_app/models.py
@@ -16,12 +16,24 @@ class Onsite_aftersales_service(models.Model):
     in_warranty = models.CharField(default='NO', max_length=30, choices=choices)
     phone_no = models.CharField(max_length=13, null=True, blank=True)
     customer_email_id = models.EmailField(max_length=255, null=True, blank=True)
-    date_of_complaint_received = models.DateField(default=datetime.date.today,blank=True)
-    customer_address = models.CharField(max_length=250, null=True, blank=True)
-    complaint_received_by = models.CharField(max_length=30, null=True, blank=True)
     nearest_railwaystation = models.CharField(max_length=30, null=True, blank=True)
     train_line = models.CharField(max_length=30, null=True, blank=True)
     products_to_be_repaired = models.CharField(max_length=30, null=True, blank=True)
+    visiting_charges_told_customer = models.CharField(max_length=30, null=True, blank=True)
+    total_cost = models.CharField(max_length=30, null=True, blank=True)
+    complaint_assigned_to = models.CharField(max_length=30, null=True, blank=True)
+    complaint_assigned_on = models.CharField(max_length=30, null=True, blank=True)
+    time_taken_destination_return_office_min = models.CharField(max_length=30, null=True, blank=True)
+    notes = models.CharField(max_length=255, null=True, blank=True)
+    feedback_given = models.CharField(max_length=30,choices=choices,default='NO')
+    auto_timedate = models.DateTimeField(default=timezone.now())
+
+    def __int__(self):
+        return self.repairingno
+
+
+class Onsite_Products(models.Model):
+    onsite_repairing_id = models.ForeignKey(Onsite_aftersales_service,on_delete=models.CASCADE)
     type_of_machine = models.CharField(max_length=30, null=True, blank=True)
     model = models.CharField(max_length=30, null=True, blank=True)
     sub_model = models.CharField(max_length=30, null=True, blank=True)
@@ -30,17 +42,6 @@ class Onsite_aftersales_service(models.Model):
     components_replaced_in_warranty = models.CharField(max_length=255, null=True, blank=True)
     components_replaced = models.CharField(max_length=255, null=True, blank=True)
     cost = models.CharField(max_length=30, null=True, blank=True)
-    visiting_charges_told_customer = models.CharField(max_length=30, null=True, blank=True)
-    total_cost = models.CharField(max_length=30, null=True, blank=True)
-    complaint_assigned_to = models.CharField(max_length=30, null=True, blank=True)
-    complaint_assigned_on = models.CharField(max_length=30, null=True, blank=True)
-    time_taken_destination_return_office_min = models.CharField(max_length=30, null=True, blank=True)
-    notes = models.CharField(max_length=255, null=True, blank=True)
-    feedback_given = models.CharField(max_length=30,choices=choices,default='NO')
 
-
-
-
-    def __str__(self):
-        return self.customer_name
-
+    def __int__(self):
+        return self.onsite_repairing_id
