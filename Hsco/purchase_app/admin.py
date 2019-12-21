01c30f163be910b074a235cb83b94870d57bb513
sagar <sagarsingh27998@gmail.com>
2019-12-21 18:01:49 +0530

    changes

--- Hsco/purchase_app/admin.py
+++ Hsco/purchase_app/admin.py
@@ -3,6 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Purchase_Details, Product_Details, Feedback
 
-admin.site.register(Purchase_Details)
-admin.site.register(Product_Details)
+class Purchase_DetailsAdmin(admin.ModelAdmin):
+
+    list_display = ('purchase_no','id')
+
+    search_fields = ('purchase_no','id')
+
+class Product_DetailsAdmin(admin.ModelAdmin):
+
+    list_display = ('purchase_id','id')
+
+    search_fields = ('purchase_id','id')
+
+
+admin.site.register(Purchase_Details , Purchase_DetailsAdmin)
+admin.site.register(Product_Details, Product_DetailsAdmin)
 admin.site.register(Feedback)
--- Hsco/restamping_app/admin.py
+++ Hsco/restamping_app/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import Restamping_after_sales_service, Restamping_Product
 
-admin.site.register(Restamping_after_sales_service)
-admin.site.register(Restamping_Product)
+class Restamping_after_sales_serviceAdmin(admin.ModelAdmin):
+
+    list_display = ('restamping_no','id')
+
+    search_fields = ('restamping_no','id')
+
+class Restamping_ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('restamping_id','id')
+
+    search_fields = ('restamping_id','id')
+
+
+admin.site.register(Restamping_after_sales_service, Restamping_after_sales_serviceAdmin)
+admin.site.register(Restamping_Product, Restamping_ProductAdmin)
