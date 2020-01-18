2216e4af0e22fb0cde61c718f3089341ef236ff5
sagar <sagarsingh27998@gmail.com>
2020-01-18 15:31:30 +0530

    length changed of every fields in model

--- Hsco/customer_app/models.py
+++ Hsco/customer_app/models.py
@@ -9,8 +9,8 @@ from django.utils import timezone
 class Customer_Details(models.Model):
     # crn_number = models.UUIDField(unique=True, default=uuid.uuid4)
     # crn_number = models.BigIntegerField(unique=True, null=True, blank=True)
-    customer_name = models.CharField(max_length=80,)
-    company_name = models.CharField(max_length=80,null=True,blank=True)
+    customer_name = models.CharField(max_length=150,)
+    company_name = models.CharField(max_length=150,null=True,blank=True)
     address = models.CharField(max_length=250,null=True,blank=True)
     contact_no = models.CharField(max_length=30,)
     customer_email_id = models.EmailField(max_length=80,null=True,blank=True)
--- Hsco/restamping_app/models.py
+++ Hsco/restamping_app/models.py
@@ -6,20 +6,20 @@ from user_app.models import SiteUser
 
 class Restamping_after_sales_service(models.Model):
     user_id = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
-    manager_id = models.CharField(max_length=60, null=True, blank=True)
+    manager_id = models.CharField(max_length=150, null=True, blank=True)
     crm_no = models.ForeignKey(Customer_Details, on_delete=models.CASCADE)
-    second_person = models.CharField(max_length=80,null=True,blank=True)
-    third_person = models.CharField(max_length=80,null=True,blank=True)
-    second_contact_no = models.CharField(max_length=80,null=True,blank=True)
-    third_contact_no = models.CharField(max_length=80,null=True,blank=True)
-    second_company_name = models.CharField(max_length=80,null=True,blank=True)
-    company_email = models.CharField(max_length=80,null=True,blank=True)
-    company_address = models.CharField(max_length=120,null=True,blank=True)
+    second_person = models.CharField(max_length=150,null=True,blank=True)
+    third_person = models.CharField(max_length=150,null=True,blank=True)
+    second_contact_no = models.CharField(max_length=150,null=True,blank=True)
+    third_contact_no = models.CharField(max_length=150,null=True,blank=True)
+    second_company_name = models.CharField(max_length=150,null=True,blank=True)
+    company_email = models.CharField(max_length=150,null=True,blank=True)
+    company_address = models.CharField(max_length=150,null=True,blank=True)
     today_date = models.DateField(default=datetime.date.today, blank=True)
     total_amount = models.FloatField(default=0.0, null=True, blank=True)
     scale_delivery_date = models.DateField(null=True, blank=True)
     entry_timedate = models.DateField(default=datetime.date.today)
-    current_stage= models.CharField(max_length=50,null=True,blank=True)
+    current_stage= models.CharField(max_length=150,null=True,blank=True)
     restamping_no = models.BigIntegerField(null=True,blank=True)
 
     restamping_start_timedate = models.DateTimeField(null=True, blank=True)
@@ -32,7 +32,7 @@ class Restamping_after_sales_service(models.Model):
 
 class Restamping_Product(models.Model):
     user_id = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
-    manager_id = models.CharField(max_length=60, null=True, blank=True)
+    manager_id = models.CharField(max_length=150, null=True, blank=True)
     restamping_id = models.ForeignKey(Restamping_after_sales_service,on_delete=models.CASCADE)
     # customer_email_id = models.EmailField(max_length=90,null=True, blank=True)
     # product_to_stampped = models.CharField(max_length=150, null=True, blank=True)
@@ -43,8 +43,8 @@ class Restamping_Product(models.Model):
     old_serial_no = models.CharField(max_length=150, null=True, blank=True)
     # old_brand = models.CharField(max_length=150, null=True, blank=True)
     amount = models.FloatField(default=0.0, null=True, blank=True)
-    new_sr_no = models.CharField(max_length=80, null=True, blank=True)
-    brand = models.CharField(max_length=50, null=True, blank=True)
+    new_sr_no = models.CharField(max_length=150, null=True, blank=True)
+    brand = models.CharField(max_length=150, null=True, blank=True)
     entry_timedate = models.DateField(default=datetime.date.today)
 
 
