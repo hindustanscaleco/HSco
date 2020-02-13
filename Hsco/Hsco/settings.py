0d388aaacc666f9d844eeb23b6ad7dce26b6e321
theambrosial <mail.ambrosial@gmail.com>
2020-02-13 07:15:12 +0000

    settings

--- Hsco/Hsco/settings.py
+++ Hsco/Hsco/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
  'default': {
      'ENGINE': 'django.db.backends.mysql',
     'NAME': 'hsco_db',
-    'USER': 'root',
+    'USER': 'root2',
     'PASSWORD': '',
     'HOST': '127.0.0.1',
     'PORT': '3306',
