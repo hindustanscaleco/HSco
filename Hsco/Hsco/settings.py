fea067e8b7a677ef147183e107261f6343255f53
theambrosial <mail.ambrosial@gmail.com>
2020-04-09 18:41:58 +0000

    settings updated

--- Hsco/Hsco/settings.py
+++ Hsco/Hsco/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'Hsco.wsgi.application'
 DATABASES = {
  'default': {
      'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'hsco_db',
-    'USER': 'root',
+    'NAME': 'hsco_db_test',
+    'USER': 'root2',
     'PASSWORD': '',
     'HOST': '127.0.0.1',
     'PORT': '3306',
