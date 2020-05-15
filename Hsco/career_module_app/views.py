8c3fda6b4ec6e6670bb64987bf930227697c7723
sagar <sagarsingh27998@gmail.com>
2020-05-16 02:34:21 +0530

    Update views.py

--- Hsco/career_module_app/views.py
+++ Hsco/career_module_app/views.py
@@ -452,7 +452,7 @@ def career_module_form_hsc(request):
                 work_exp.company_name = company_name
                 work_exp.career_id = Career_module.objects.get(id=item.id)
                 work_exp.save()
-        messages.success(request, "Thank You For Interest, Your application no is "+application_number+". Our Team Will Get In Touch With You Soon!!!")
+        messages.success(request, "Thank You For Interest, Your application no is "+str(application_number)+". Our Team Will Get In Touch With You Soon!!!")
 
 
 
