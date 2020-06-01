a30e78944d3a97b4df5fbb874bee0fa3c7c255ca
theambrosial <vikas.pandey9323@gmail.com>
2020-06-02 03:02:04 +0530

    Update views.py

--- Hsco/lead_management/views.py
+++ Hsco/lead_management/views.py
@@ -55,13 +55,10 @@ def lead_home(request):
     last_date = IndiamartLeadDetails.objects.latest('to_date').to_date.strftime('%d-%b-%Y')
     from_date = last_date
     import datetime
-    print(from_date)
-    print(from_date)
-    # to_date = datetime.datetime.today() + datetime.timedelta(days=1)
-    to_date = IndiamartLeadDetails.objects.latest('to_date').to_date + datetime.timedelta(days=5)
+    to_date = datetime.datetime.today().strftime('%d-%b-%Y')
     print('to_date')
     print(to_date)
-    to_date= to_date.strftime('%d-%b-%Y')
+
     print(to_date)
     from datetime import datetime
 
