216a7613ccc953b8a561371d2b88b414b04b9c36
theambrosial <47419028+theambrosial@users.noreply.github.com>
2020-12-21 21:15:48 +0530

    Update views.py

--- Hsco/expense_app/views.py
+++ Hsco/expense_app/views.py
@@ -590,10 +590,10 @@ def expense_report_dashboard(request):
     return render(request,'expense_app/expense_report_dashboard.html')
 
 def expense_master(request):
-    expense_masters = Expense_Type_Sub_Sub_Master.objects.all()
-    context={
-        'expense_masters' : expense_masters,
-    }
+    # expense_masters = Expense_Type_Sub_Sub_Master.objects.all()
+    # context={
+    #     'expense_masters' : expense_masters,
+    # }
     return render(request,'expense_app/expense_master.html', context)
 
 def expense_type_sub_master(request):
