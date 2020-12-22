6c86dba9a9e0368717de11fcf9e0238c39d8e30c
theambrosial <47419028+theambrosial@users.noreply.github.com>
2020-12-22 12:40:24 +0530

    removed expense

--- Hsco/expense_app/admin.py
+++ Hsco/expense_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Expense_Type_Sub_Master, Expense_Type_Sub_Sub_Master, Vendor, Expense, Expense_Product
-# Register your models here.
+# from .models import Expense_Type_Sub_Master, Expense_Type_Sub_Sub_Master, Vendor, Expense, Expense_Product
+# # Register your models here.
 
-admin.site.register(Expense_Type_Sub_Master)
-admin.site.register(Expense_Type_Sub_Sub_Master)
-admin.site.register(Vendor)
-admin.site.register(Expense)
-admin.site.register(Expense_Product)
+# admin.site.register(Expense_Type_Sub_Master)
+# admin.site.register(Expense_Type_Sub_Sub_Master)
+# admin.site.register(Vendor)
+# admin.site.register(Expense)
+# admin.site.register(Expense_Product)
