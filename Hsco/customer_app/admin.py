d0ca826461f448204e2d539d56c265682e63bbc7
theambrosial <vikas.pandey9323@gmail.com>
2021-01-08 12:31:12 +0530

    from p3 doc

--- Hsco/customer_app/admin.py
+++ Hsco/customer_app/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import type_purchase,main_model,sub_model,sub_sub_model, Log
+from import_export.admin import ImportExportModelAdmin
+
+from .models import type_purchase,main_model,sub_model,sub_sub_model, Log,DynamicDropdown
 
 # Register your models here.
 from .models import Customer_Details,Lead_Customer_Details
+
+
+class DDAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'type', 'is_enabled','entry_timedate',)
+    list_filter = ( 'type','is_enabled',)
+    search_fields = ('name',)
 
 admin.site.register(Customer_Details)
 admin.site.register(Lead_Customer_Details)
@@ -11,4 +19,5 @@ admin.site.register(main_model)
 admin.site.register(sub_model)
 admin.site.register(sub_sub_model)
 admin.site.register(Log)
+admin.site.register(DynamicDropdown,DDAdmin)
 
