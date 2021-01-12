cca9878ab3ddd19b5d2c7d983c55217199b6db45
theambrosial <vikas.pandey9323@gmail.com>
2021-01-12 12:46:53 +0530

    Update views.py

--- Hsco/dispatch_app/views.py
+++ Hsco/dispatch_app/views.py
@@ -180,7 +180,7 @@ def one_time_dd():
             if dispatch_id.channel_of_dispatch == 'Franchisee StoreSure' or dispatch_id.channel_of_dispatch == 'Franchisee StoreTejas' or \
                     dispatch_id.channel_of_dispatch == 'Franchisee StoreBhairav Jwellers' or dispatch_id.channel_of_dispatch == 'Franchisee Storecas' or dispatch_id.channel_of_dispatch == 'Franchisee Storecasc':
                 refine_s = 'Franchisee Store'
-            if dispatch_id.channel_of_dispatch == 'Taxi' or dispatch_id.channel_of_dispatch == 'wefast' or dispatch_id.channel_of_dispatch == 'Wefast':
+            if dispatch_id.channel_of_dispatch == 'Taxi' or dispatch_id.channel_of_dispatch == 'wefast' or dispatch_id.channel_of_dispatch == 'Wefast' or dispatch_id.channel_of_dispatch == 'Sanjay Transport':
                 refine_s = 'Transport'
 
             if dispatch_id.channel_of_dispatch == 'By Hand' or dispatch_id.channel_of_dispatch == 'Self':
