3fe567ddb13e436d1af6929ed0243fcfbb00b8eb
Harsh <harshkumarpathak01@gmail.com>
2021-02-12 16:52:34 +0530

    Initial Commit for updated ui

--- Hsco/Hsco/settings.py
+++ Hsco/Hsco/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
  'default': {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'hsco_db',
-    'USER': 'root2',
+    'USER': 'root',
     'PASSWORD': '',
     'HOST': '127.0.0.1',
     'PORT': '3306',
