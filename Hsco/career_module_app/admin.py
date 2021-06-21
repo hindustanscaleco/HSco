a28cacd15d7f217a19e65ff30372e4382881c141
sagarsingh <sagarsingh27998@gmail.com>
2021-06-21 18:57:53 +0530

    bill and expense in add user and cgst sgst amounts in expense

--- Hsco/career_module_app/admin.py
+++ Hsco/career_module_app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Career_module, EducationalDetails, WorkExperience, Position
 # Register your models here.
-admin.site.register(Career_module)
+class Career_moduleAdmin(admin.ModelAdmin):
+
+    list_display = ('candidate_name','id','phone_no')
+
+    search_fields = ('candidate_name','id','phone_no')
+
+admin.site.register(Career_module, Career_moduleAdmin)
 admin.site.register(EducationalDetails)
 admin.site.register(WorkExperience)
 admin.site.register(Position)
