1b87e789a4ce3063cc68410bc23803d429fdbda6
sagarsingh <sagarsingh27998@gmail.com>
2021-06-24 11:17:52 +0530

    Update admin.py

--- Hsco/expense_app/admin.py
+++ Hsco/expense_app/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Bill, Expense_Type_Sub_Master, Expense_Type_Sub_Sub_Master, Vendor, Expense, Expense_Product
 # Register your models here.
+class ExpenseAdmin(admin.ModelAdmin):
+
+    list_display = ('expense_type_sub_sub_master_id','id')
+
+    search_fields = ('expense_type_sub_sub_master_id','id')
 
 admin.site.register(Bill)
 admin.site.register(Expense_Type_Sub_Master)
 admin.site.register(Expense_Type_Sub_Sub_Master)
 admin.site.register(Vendor)
-admin.site.register(Expense)
+admin.site.register(Expense,ExpenseAdmin)
 admin.site.register(Expense_Product)
