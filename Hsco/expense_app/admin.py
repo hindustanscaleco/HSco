4c6d7df2471bf24f44342f1bea1860f875ac4b3f
sagarsingh <sagarsingh27998@gmail.com>
2021-06-24 11:22:57 +0530

    Update admin.py

--- Hsco/expense_app/admin.py
+++ Hsco/expense_app/admin.py
@@ -7,8 +7,14 @@ class ExpenseAdmin(admin.ModelAdmin):
 
     search_fields = ('expense_type_sub_sub_master_id','id')
 
+class Expense_Type_Sub_MasterAdmin(admin.ModelAdmin):
+
+    list_display = ('expense_type_master','id')
+
+    search_fields = ('expense_type_master','id')
+
 admin.site.register(Bill)
-admin.site.register(Expense_Type_Sub_Master)
+admin.site.register(Expense_Type_Sub_Master, Expense_Type_Sub_MasterAdmin)
 admin.site.register(Expense_Type_Sub_Sub_Master)
 admin.site.register(Vendor)
 admin.site.register(Expense,ExpenseAdmin)
