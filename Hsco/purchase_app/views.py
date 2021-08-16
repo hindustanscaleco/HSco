b2ea32187ceb203eab54de6a89b760829611e5e9
Sagar Singh <sagarsingh27998@gmail.com>
2021-08-16 13:13:15 +0530

    Update views.py

--- Hsco/purchase_app/views.py
+++ Hsco/purchase_app/views.py
@@ -2820,7 +2820,7 @@ def modules_map(request):
         if 'map_all_data' in request.POST:
             import requests
             customer_list = Customer_Details.objects.filter(
-                latitude=None, longitude=None).values_list('address').distinct()[0:100]
+                latitude=None, longitude=None).values_list('address').distinct()[100:]
 
             geo_api_key = 'AIzaSyAX9a8Sct4E4LN-P0MTJoKzb4iqYodyWdo'
 
